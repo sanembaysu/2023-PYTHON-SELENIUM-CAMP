0dade75e61c18d3e28754425df35aceb2a57a21b
Sanem Baysu <82761726+sanembaysu@users.noreply.github.com>
2023-03-18 20:41:16 +0300

    Add files via upload

---
+++ DAY 2/hw2/classes.py
@@ -0,0 +1,9 @@
+class Banka:
+    def krediBasvur(self):                          # self -> this (java)
+        print("Kredi Basvurusu Yapildi")
+
+    def krediHesapla(self):
+        print("Hesaplar yapildi")
+
+banka = Banka()
+banka.krediBasvur()
---
+++ DAY 2/hw2/person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
+musteri1 = Person("Ahmet","Demirog")
+musteri2 = Person("Kerem","Varis")
+musteri3 = Person("Ilker","Tural")
+
+print(musteri1.name)
