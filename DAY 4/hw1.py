ff21a3055b34c0cfeac2234a1bee4a762ee02ddd
Sanem Baysu <82761726+sanembaysu@users.noreply.github.com>
2023-03-22 23:27:40 +0300

    Add files via upload

---
+++ DAY 4/hw1.py
@@ -0,0 +1,36 @@
+"""
+HTML
+    HTML, web tasarimi yapmaya yarar. Tasarimmcilar icin yaoi profilleri, baglantilar,
+    paragraflar ve basliklar olusturmaya yardimci olur. BU sekilde web sayfalari sekillendirilir.
+    Bir programlama dili degildir yani HTML kodlari ile tke basina calisan kodlar yazilamaz.
+
+"""
+
+"""
+HTML Locators
+    Konumlandiricilar, Selenium IDE'ye hangi web tabanli objeler uzerinde calismasi gerektigini
+    soyleyen bir komuttur. Dogru elementin tanimlanmasi, otomasyon oluşturmanin on kosuludur.
+    Ornegin site uzerindeki bir elemente giris butonuna Selenium ile tiklama islemi yaptirmak
+    istendiginde islemi konumlandiricilar ile yapariz.
+
+    Locator Cesitleri:
+    - ID
+    - Name
+    - XPath
+    - TagName
+    - LinkText
+    - ClassName
+    - CssSelector
+
+
+"""
+
+"""
+Selenium'da Aksiyonlar:
+- click                         ->  Bir nesneye tiklamak içindir.
+- send_keys()                   ->  Bu aksiyon giris iceriklerine karsi metin gönderebiliriz.
+- find_element_by_id()          ->  Bu aksiyon ile 'id' ozniteligine sahip bir degeri seçebiliriz.
+- find_element_by_name()        ->  Bu aksiyon ile 'name' ozniteligine sahip bir degeri seçebiliriz.
+- find_element_by_class_name()  ->  Bu aksiyon ile 'class' ozniteligine sahip bir degeri seçebiliriz. 
+
+"""
---
+++ DAY 4/hw2.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.common.by import By
+
+#from Mart18_Ders4 import Test_Sauce
+
+class Test_Sauce : 
+
+    def test_login(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+        driver.get("https://www.saucedemo.com/")
+        sleep(5)
+        username = driver.find_element(By.ID, "user-name")
+        password= driver.find_element(By.ID,"password")
+        sleep(2)
+        username.send_keys("1")
+        password.send_keys("1")
+        sleep(2)
+        loginBtn = driver.find_element(By.ID,"login-button")
+        sleep(2)
+        loginBtn.click()
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
+        print(f"TEST SONUCU: {testResult}")
+
+    def test_username(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+        driver.get("https://www.saucedemo.com/")
+        sleep(5)
+        loginBtn = driver.find_element(By.ID,"login-button")
+        sleep(2)
+        loginBtn.click()
+        sleep(5)
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface:  Username is required"
+        print(f"TEST SONUCU: {testResult}")
+
+    def test_password(self):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+        driver.get("https://www.saucedemo.com/")
+        sleep(5)
+        username = driver.find_element(By.ID, "user-name")
+        sleep(2)
+        username.send_keys("1")
+        sleep(2)
+        loginBtn = driver.find_element(By.ID,"login-button")
+        sleep(2)
+        loginBtn.click()
+        sleep(5)
+        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface:  Password is required"
+        print(f"TEST SONUCU: {testResult}")
+
+testClass = Test_Sauce()
+testClass.test_password()
